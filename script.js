0d83f8d33dcd4dbfeff065d976e6963c345433a5
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-09-25 18:15:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+// Get references to the text box and the word count display element
+const evaluatedText = document.getElementById('evaluatedText');
+const wordCount = document.getElementById('wordCount');
+
+// Add an input event listener to the text box
+evaluatedText.addEventListener('input', () => {
+    // Get the text from the text box
+    const text = evaluatedText.value;
+
+    // Split the text into words using whitespace as the delimiter
+    const words = text.trim().split(/\s+/);
+
+    // Filter out empty strings from the array
+    const nonEmptyWords = words.filter((word) => word !== '');
+
+    // Update the word count display with the count of non-empty words
+    wordCount.textContent = nonEmptyWords.length;
+});
+
+
